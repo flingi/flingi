def89ca88cece23fc7702340df7c6cb056205330
flingi <prawin.nageswaran@gmail.com>
2017-05-07 23:41:56 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,38 @@
+var Excel = require("exceljs");
+
+
+  
+
+
+ var filename= "D:/dump/new.xlsx"; // existing filepath
+  var workbook = new Excel.Workbook();
+
+workbook.xlsx.readFile(filename)
+    .then(function() {
+        var worksheet = workbook.getWorksheet(1);
+    worksheet.columns = [
+    { header: 'Id',  width: 10 },
+    { header: 'Name', width: 32 },
+    { header: 'D.O.B.', width: 10 }
+];
+        var size=worksheet.rowCount+1;
+        console.log(size)
+      var row = worksheet.getRow(size);
+        row.getCell(1).value = scenario();
+        row.getCell(2).value=name();
+        row.commit();
+         return workbook.xlsx.writeFile('D:/dump/new.xlsx');
+      })
+
+
+function scenario(){
+var scenario="SAO 41: test"
+return scenario;
+}
+
+function name()
+{
+
+var name="praveen"
+return name;
+}
